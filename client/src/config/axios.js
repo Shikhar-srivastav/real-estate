5c502b4670351e8f4be1ac260c84588c47ac5413
Shikhar Srivastava <shikharsr100@gmail.com>
2023-07-12 20:36:59 +0530

    Changes to client

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://real-estate-api-nh3y.onrender.com/",
+  baseURL: "https://real-estate-api-nh3y.onrender.com",
 });
 
-export default api;
+export default api
